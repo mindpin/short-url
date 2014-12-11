57acec52d3378c87c9b58f80650e76764501fc6d
fushang318 <fushang318@gmail.com>
2014-12-11 17:19:31 +0800

    add log file

--- lib/app.rb
+++ lib/app.rb
@@ -22,8 +22,23 @@ require 'carrierwave-aliyun'
 require File.expand_path("../../config/env",__FILE__)
 require "./lib/image_uploader"
 require "./lib/short_url"
+require 'logger'
 
 class ShortUrlApp < Sinatra::Base
+  ::Logger.class_eval { alias :write :'<<' }
+  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'..','tmp','logs','access.log')
+  access_logger = ::Logger.new(access_log)
+  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'..','tmp','logs','error.log'),"a+")
+  error_logger.sync = true
+ 
+  configure do
+    use ::Rack::CommonLogger, access_logger
+  end
+ 
+  before {
+    env["rack.errors"] =  error_logger
+  }
+
   configure :development do
     register Sinatra::Reloader
   end
